b0eac553e5211cb96ba13b9f4c551b18ae43db5b
songhuarong <songhr@ebpoo.com>
2018-04-23 14:52:30 +0800

    划分项目结构和集成Cocopod

---
+++ YingKe_OC/YingKe_OC/Class/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YingKe_OC
+//
+//  Created by huarong on 2018/4/23.
+//  Copyright © 2018年 huarong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
